d404b475df0a60aff2f02f8987f96ed18ac095ed
pearbender <pearbender@gmail.com>
2024-04-02 05:17:20 +0900

    Add sample flask app

---
+++ app.py
@@ -0,0 +1,46 @@
+from threading import Lock
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import random
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
+
+
+def background_thread():
+    """Example of how to send server generated events to clients."""
+    count = 0
+    while True:
+        socketio.sleep(1)
+        count += 1
+        data = {
+            'labels': ['A', 'B', 'C', 'D', 'E'],
+            'values': [random.randint(1, 10) for _ in range(5)]
+        }
+        socketio.emit('my_response',
+                      data)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', async_mode=socketio.async_mode)
+
+
+@socketio.event
+def connect():
+    global thread
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(background_thread)
+
+
+if __name__ == '__main__':
+    socketio.run(app)
